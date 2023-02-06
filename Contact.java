57ab1b62cff0244a6250578d626c101b3c134b04
Shankar Gavkare <102272115+gavkare@users.noreply.github.com>
2023-02-07 00:04:43 +0530

    Add files via upload

--- Contact.java
+++ Contact.java
@@ -1,63 +1,76 @@
-package Day10;
+package Day9;
 
 public class Contact {
-    {
-        public String firstName;
-        public String phoneNumber;
-        public String address ;
-        public String city ;
-        public String state ;
-        public String email;
-        public String LastName;
-        public String Zip;
+    public String firstName;
+    public String phoneNumber;
+    public String address;
+    public String city;
+    public String state;
+    public String email;
+    public String LastName;
+    public String Zip;
 
-        public String getFirstName() {
+    public String getFirstName() {
         return firstName;
     }
-        public void setFirstName(String firstName) {
+
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-        public String getLastName() {
+
+    public String getLastName() {
         return LastName;
     }
-        public void setLastName(String lastName) {
+
+    public void setLastName(String lastName) {
         this.LastName = lastName;
     }
-        public String getPhoneNumber() {
+
+    public String getPhoneNumber() {
         return phoneNumber;
     }
-        public void setPhoneNumber(String phoneNumber) {
+
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-        public String getAddress() {
+
+    public String getAddress() {
         return address;
     }
-        public void setAddress(String address) {
+
+    public void setAddress(String address) {
         this.address = address;
     }
-        public String getCity() {
+
+    public String getCity() {
         return city;
     }
-        public void setCity(String city) {
+
+    public void setCity(String city) {
         this.city = city;
     }
-        public String getState() {
+
+    public String getState() {
         return state;
     }
-        public void setState(String state) {
+
+    public void setState(String state) {
         this.state = state;
     }
-        public String getZip() {
+
+    public String getZip() {
         return Zip;
     }
-        public void setZip(String zip) {
+
+    public void setZip(String zip) {
         this.Zip = zip;
     }
-        public String getEmail() {
+
+    public String getEmail() {
         return email;
     }
-        public void setEmail(String email) {
+
+    public void setEmail(String email) {
         this.email = email;
-    }
     }
 }
